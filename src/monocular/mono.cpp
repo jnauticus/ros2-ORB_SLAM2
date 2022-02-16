debac681ec32ed6a8a6df0dd0883726f2a3282eb
jnauticus <98918161+jnauticus@users.noreply.github.com>
2022-02-16 14:29:12 -0600

    update OS3

--- src/monocular/mono.cpp
+++ src/monocular/mono.cpp
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
     // malloc error using new.. try shared ptr
     // Create SLAM system. It initializes all system threads and gets ready to process frames.
     bool visualization = true;
-    ORB_SLAM2::System SLAM(argv[1], argv[2], ORB_SLAM2::System::MONOCULAR, visualization);
+    ORB_SLAM3::System SLAM(argv[1], argv[2], ORB_SLAM3::System::MONOCULAR, visualization);
 
     auto node = std::make_shared<MonocularSlamNode>(&SLAM);
 
